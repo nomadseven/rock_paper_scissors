d554580495360a253b30cc5614288eda1ae1c143
Adam Cutter <158004553+nomadseven@users.noreply.github.com>
2024-03-16 12:32:16 -0500

    Repair script doc with strict equalities

--- script.js
+++ script.js
@@ -3,48 +3,47 @@ function getComputerChoice() {
     const select = choice[Math.floor(Math.random() * choice.length)];
     return select;
 }
-
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection = "ROCK") && (computerSelection = "ROCK")) {
+    if ((playerSelection === "ROCK") && (computerSelection === "ROCK")) {
         message = "You tied!";
     }
 
-    else if ((playerSelection = "ROCK") && (computerSelection = "PAPER")) {
+    else if ((playerSelection === "ROCK") && (computerSelection === "PAPER")) {
         message = "You lose! Paper beats rock.";
     }
 
-    else if ((playerSelection = "ROCK") && (computerSelection = "SCISSORS")) {
+    else if ((playerSelection === "ROCK") && (computerSelection === "SCISSORS")) {
         message = "You win! Rock beats paper.";
     }
 
-    else if ((playerSelection = "PAPER") && (computerSelection = "ROCK")) {
+    else if ((playerSelection === "PAPER") && (computerSelection === "ROCK")) {
         message = "You win! Paper beats rock.";
     }
 
-    else if ((playerSelection = "PAPER") && (computerSelection = "PAPER")) {
+    else if ((playerSelection === "PAPER") && (computerSelection === "PAPER")) {
         message = "You tied!";
     }
 
-    else if ((playerSelection = "PAPER") && (computerSelection = "SCISSORS")) {
+    else if ((playerSelection === "PAPER") && (computerSelection === "SCISSORS")) {
         message = "You lose! Scissors beat paper.";
     }
 
-    else if ((playerSelection = "SCISSORS") && (computerSelection = "ROCK")) {
+    else if ((playerSelection === "SCISSORS") && (computerSelection === "ROCK")) {
         message = "You lose! Rock beats scissors.";
     }
 
-    else if ((playerSelection = "SCISSORS") && (computerSelection = "PAPER")) {
+    else if ((playerSelection === "SCISSORS") && (computerSelection === "PAPER")) {
         message = "You win! Scissors beats paper.";
     }
 
-    else if ((playerSelection = "SCISSORS") && (computerSelection = "SCISSORS")) {
+    else if ((playerSelection === "SCISSORS") && (computerSelection === "SCISSORS")) {
         message = "You tied!";
     }
 
     return message;
 }
 
-let playerSelection = "rock";
+let playerSelection = "paper";
 playerSelection = playerSelection.toUpperCase();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
